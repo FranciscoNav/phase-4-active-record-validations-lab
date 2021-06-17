92c1ba440e0d3ac2fbca848442fe833b62761d6d
Francisco <fnaveira11@gmail.com>
2021-06-17 19:20:34 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['fiction' 'non-fiction'] }
+
+    # validates :clickbait-y
+
+    # def clickbait-y
+    #     if :title.present?
+    #         inclusion: { in: %w(fiction non-fiction) }
+    #     end
+    # end
+
 end
